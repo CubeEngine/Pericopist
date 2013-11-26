b7c8b120ec2b395b6657d709cf9f56579297346c
Stefan Wolf <boeserwolf91@gmail.com>
2013-11-26 12:30:55 +0100

    started to use the eclipse jdt parser

--- src/main/java/de/cubeisland/maven/plugins/messagecatalog/parser/java/SourceClassVisitor.java
+++ src/main/java/de/cubeisland/maven/plugins/messagecatalog/parser/java/SourceClassVisitor.java
@@ -2,14 +2,9 @@ package de.cubeisland.maven.plugins.messagecatalog.parser.java;
 
 import de.cubeisland.maven.plugins.messagecatalog.parser.Occurrence;
 import de.cubeisland.maven.plugins.messagecatalog.parser.TranslatableMessage;
-import japa.parser.ast.ImportDeclaration;
-import japa.parser.ast.expr.Expression;
-import japa.parser.ast.expr.MemberValuePair;
-import japa.parser.ast.expr.MethodCallExpr;
-import japa.parser.ast.expr.NameExpr;
-import japa.parser.ast.expr.NormalAnnotationExpr;
-import japa.parser.ast.expr.StringLiteralExpr;
-import japa.parser.ast.visitor.VoidVisitorAdapter;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
 
 import java.io.File;
 import java.util.HashMap;
@@ -18,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-class SourceClassVisitor extends VoidVisitorAdapter<File>
+class SourceClassVisitor extends ASTVisitor
 {
 
     private final HashMap<String, Set<String>> annotations = new HashMap<String, Set<String>>()
@@ -43,12 +38,14 @@ class SourceClassVisitor extends VoidVisitorAdapter<File>
         }
     };
 
+    private final CompilationUnit compilationUnit;
     private final String basePackage;
     private final Map<String, TranslatableMessage> messages;
     private final Map<String, String> importedClasses;
 
-    public SourceClassVisitor(String basePackage)
+    public SourceClassVisitor(CompilationUnit compilationUnit, String basePackage)
     {
+        this.compilationUnit = compilationUnit;
         this.basePackage = basePackage;
         this.messages = new HashMap<String, TranslatableMessage>();
         this.importedClasses = new HashMap<String, String>();
@@ -73,56 +70,63 @@ class SourceClassVisitor extends VoidVisitorAdapter<File>
     }
 
     @Override
-    public void visit(ImportDeclaration n, File file)
+    public boolean visit(ImportDeclaration node)
     {
-        if (!n.isStatic() && !n.isAsterisk())
-        {
-            final NameExpr nameExpr = n.getName();
-            final String fqcn = nameExpr.toString();
-            if (fqcn.startsWith(basePackage))
-            {
-                this.importedClasses.put(nameExpr.getName(), fqcn);
-            }
-        }
-        super.visit(n, file);
+        System.out.println(node.getName());
+        return super.visit(node);
     }
 
-    @Override
-    public void visit(NormalAnnotationExpr n, File file)
-    {
-        Set<String> fields = this.annotations.get(n.getName().getName());
-        if (fields != null)
-        {
-            for (MemberValuePair pair : n.getPairs())
-            {
-                if (fields.contains(pair.getName()))
-                {
-                    Expression expr = pair.getValue();
-                    if (expr instanceof StringLiteralExpr)
-                    {
-                        this.addMessage(((StringLiteralExpr)expr).getValue(), new Occurrence(file, expr.getBeginLine()));
-                    }
-                }
-            }
-        }
-        super.visit(n, file);
-    }
-
-    @Override
-    public void visit(MethodCallExpr n, File file)
-    {
-        if (this.methods.contains(n.getName()))
-        {
-            List<Expression> args = n.getArgs();
-            if (args.size() > 0)
-            {
-                Expression expr = args.get(0);
-                if (expr instanceof StringLiteralExpr)
-                {
-                    this.addMessage(((StringLiteralExpr)expr).getValue(), new Occurrence(file, expr.getBeginLine()));
-                }
-            }
-        }
-        super.visit(n, file);
-    }
+    //    @Override
+//    public void visit(ImportDeclaration n, File file)
+//    {
+//        if (!n.isStatic() && !n.isAsterisk())
+//        {
+//            final NameExpr nameExpr = n.getName();
+//            final String fqcn = nameExpr.toString();
+//            if (fqcn.startsWith(basePackage))
+//            {
+//                this.importedClasses.put(nameExpr.getName(), fqcn);
+//            }
+//        }
+//        super.visit(n);
+//    }
+//
+//    @Override
+//    public void visit(NormalAnnotationExpr n, File file)
+//    {
+//        Set<String> fields = this.annotations.get(n.getName().getName());
+//        if (fields != null)
+//        {
+//            for (MemberValuePair pair : n.getPairs())
+//            {
+//                if (fields.contains(pair.getName()))
+//                {
+//                    Expression expr = pair.getValue();
+//                    if (expr instanceof StringLiteralExpr)
+//                    {
+//                        this.addMessage(((StringLiteralExpr)expr).getValue(), new Occurrence(file, expr.getBeginLine()));
+//                    }
+//                }
+//            }
+//        }
+//        super.visit(n, file);
+//    }
+//
+//    @Override
+//    public void visit(MethodCallExpr n, File file)
+//    {
+//        if (this.methods.contains(n.getName()))
+//        {
+//            List<Expression> args = n.getArgs();
+//            if (args.size() > 0)
+//            {
+//                Expression expr = args.get(0);
+//                if (expr instanceof StringLiteralExpr)
+//                {
+//                    this.addMessage(((StringLiteralExpr)expr).getValue(), new Occurrence(file, expr.getBeginLine()));
+//                }
+//            }
+//        }
+//        super.visit(n, file);
+//    }
 }
