e2bfb744fd86ad0d3a3b9d1d25410b34d7158b0f
Stefan Wolf <boeserwolf91@gmail.com>
2014-03-04 20:41:06 +0100

    set ./src/main/java as the default source file directory

--- src/main/java/de/cubeisland/maven/plugins/messagecatalog/parser/AbstractSourceConfiguration.java
+++ src/main/java/de/cubeisland/maven/plugins/messagecatalog/parser/AbstractSourceConfiguration.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlElement;
 public abstract class AbstractSourceConfiguration implements SourceConfiguration
 {
     @XmlElement
-    protected File directory;
+    protected File directory = new File("./src/main/java");
 
     public File getDirectory()
     {
