d184b3bd868abc9015323ef6a5774af386289e4a
Phillip Schichtel <phillip.public@schich.tel>
2017-05-24 22:29:44 +0200

    More license header updates

--- maven-plugin/src/main/java/org/cubeengine/maven/plugins/pericopist/mojo/UpdateMojo.java
+++ maven-plugin/src/main/java/org/cubeengine/maven/plugins/pericopist/mojo/UpdateMojo.java
@@ -1,18 +1,19 @@
-/**
- * This file is part of pericopist-maven-plugin.
+/*
+ * pericopist-maven-plugin - A maven plugin to extract messages from your source code and generate message catalogs.
+ * Copyright © 2013 Cube Island (development@cubeisland.de)
  *
- * pericopist-maven-plugin is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * pericopist-maven-plugin is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with pericopist-maven-plugin.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.cubeengine.maven.plugins.pericopist.mojo;
 
